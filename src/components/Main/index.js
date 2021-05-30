8f5f8d53973693f401f18a52a64e525586db0d51
Juan Manuel <juan_man.becerra@uao.edu.co>
2021-05-30 15:06:40 -0500

    estilos

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import socketIOClient, { io } from 'socket.io-client'
+import './style.css'
+
 const ENDPOINT = 'industrial.ubidots.com:9012'
 const TOKEN = 'BBFF-m6cylBBBG5xN0xY0krNtMY6jVfYqEt'
 const DEVICE_LABEL = 'test'
@@ -21,20 +23,44 @@ function App() {
 
   return (
     <main>
-      <h1 className="title">Estado de caja fuerte:</h1>
-      <section className="info-section">
-        <p>
-          Puerta: <span>{}</span>
-        </p>
-        <p>
-          Temperatura: <span>{}</span>
-        </p>
+      <section className="box-state">
+        <h1 className="title">Estado de caja fuerte:</h1>
+        <div className="state">Segura</div>
       </section>
-      <section className="map-section">
-        <iframe src="" frameborder="0"></iframe>
-      </section>
-      <section className="status-section"></section>
-      <section className="img-section"></section>
+
+      <div className="smart-info">
+        <section className="info-section">
+          <p>
+            Puerta: <span>{ }</span>
+          </p>
+          <p>
+            Temperatura: <span>{ }</span>
+          </p>
+        </section>
+        <section className="map-section">
+          <iframe
+            width="100%"
+            height="100%"
+            center="3.488590, -76.516390"
+            loading="lazy"
+            allowfullscreen
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBC9qr456dF_UWRtj6joKqoqzxf8EAal-Q&q=3.488590, -76.516390">
+          </iframe>
+        </section>
+      </div>
+
+      <div className="smart-state">
+        <section className="status-section">
+          <div>
+            <h2>Haz ingresado la contraseña correcta</h2>
+            <h1>¿Deseas confirmar la apertura?</h1>
+            <button>Aceptar</button>
+            <button>Rechazar</button>
+          </div>
+        </section>
+        <section className="img-section">image</section>
+      </div>
+
     </main>
   )
 }
